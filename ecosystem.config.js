b6f6abdc1add240b8114f9c9cfabc302ec12f708
nbdhhzh <zhihuan.huang@pku.edu.cn>
2025-12-29 18:19:52 +0800

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "chateval",
-    script: "./ChatEval/bin/python",
+    script: "./backend/ChatEval/bin/python",
     cwd: __dirname,
     args: "-m uvicorn backend.main:app --host 127.0.0.1 --port 8001",
     interpreter: "none"
